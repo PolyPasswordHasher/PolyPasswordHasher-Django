bcee9073d7a622ca8e18b474a879925f24bc555d
Santiago Torres <torresariass@gmail.com>
2014-06-24 12:02:49 -0400

    Updated the administrator command
    The administrator command now sets the thresholdless key as part of the
    initialization routine.

--- django_pph/management/commands/initialize_pph_context.py
+++ django_pph/management/commands/initialize_pph_context.py
@@ -8,6 +8,7 @@ from base64 import b64encode
 
 
 from django_pph.shamirsecret import ShamirSecret
+from django.conf import settings
 
 
 
@@ -27,8 +28,6 @@ class Command(BaseCommand):
         # a shamir secret object and assign the secret to the thresholdlesskey
         self.initialize(hasher)
 
-        # TODO: are we missing anything else here?
-        #
 
         # return
 
@@ -38,16 +37,19 @@ class Command(BaseCommand):
       assert hasher.verify_secret(hasher.secret) == True
       hasher.shamirsecretobj = ShamirSecret(hasher.threshold, hasher.secret)
       hasher.thresholdlesskey = hasher.secret
+      hasher.nextavailableshare = 1
       hasher.is_unlocked = True
 
     # create_secret method:
     #   returns a random string consisting of 28 bytes of random data
     #   and 4 bytes of hash to verify the secret upon recombination
     def create_secret(self):
-        secret = get_random_string(28)
+        secret = get_random_string(settings.PPH_SECRET_LENGTH - 
+                settings.PPH_SECRET_VERIFICATION_BYTES)
         secret_digest = self.digest(secret).digest()
-        secret_digest = b64encode(secret_digest).decode('ascii').strip()[:4]
-        secret += secret_digest
+        secret_digest = b64encode(secret_digest).decode('ascii').strip()
+        secret += secret_digest[:settings.PPH_SECRET_VERIFICATION_BYTES]
+
         return bytes(secret)
 
 

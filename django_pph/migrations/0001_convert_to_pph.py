386632f1a4062016a2a9e1c21a7e79111c7d7785
Santiago Torres <torresariass@gmail.com>
2014-07-10 14:39:50 -0400

    Added base migration script

---
+++ django_pph/migrations/0001_convert_to_pph.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import DataMigration
+from django.db import models
+
+from django.contrib.auth.hashers import get_hasher
+from django.core import management
+import django_pph
+
+
+class Migration(DataMigration):
+
+    def forwards(self, orm):
+        # Note: Don't use "from appname.models import ModelName". 
+        # Use orm.ModelName to refer to models in this application,
+        # and orm['appname.ModelName'] for models in other applications.
+	
+        polypasshasher = get_hasher('pph')
+        management.call_command('initialize_pph_context')
+        next_available_share = 1
+	
+        for user in orm.User.objects.all():
+            print("==={}".format(user.username))
+            # TODO: IDENTIFY THE ALGORITHM FIRST. I am assuming this is a
+            # pbkdf2_sha256 for the time being.
+            algorithm, iterations, salt, hash = user.password.split('$')
+            
+            assert(algorithm=='pbkdf2_sha256')
+
+            if user.is_superuser:
+            # sharenumber = hasher.data['nextavailableshare']
+            # hasher.data['nextavailableshare']
+            # hasher.update()
+            # newhash = hasher._polyhash_entry(hash
+                new_hash, sharenumber = polypasshasher.update_hash_threshold(hash)
+                new_password = "{}${}${}${}${}".format('pph',sharenumber,
+                        iterations, salt, new_hash)
+                print("threshold: {} -> {}".format(hash, new_hash))
+            else:
+                new_hash = polypasshasher.update_hash_thresholdless(hash)
+                new_password = "{}${}${}${}${}".format('pph',0,iterations,
+                    salt, new_hash)
+                print("thresholdless: {} -> {}".format(hash, new_hash))
+                
+            user.password = new_password
+            user.last_login = datetime.datetime.today()
+            user.date_joined = datetime.datetime.today()
+            user.save()
+
+       # and orm['appname.ModelName'] for models in other applications.
+
+    def backwards(self, orm):
+        "Write your backwards methods here."
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['auth']
+    symmetrical = True

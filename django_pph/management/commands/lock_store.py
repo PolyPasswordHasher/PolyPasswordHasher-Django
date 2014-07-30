2aed00bd7593e45451be6ce2a4ac184a764d0396
Santiago Torres <torresariass@gmail.com>
2014-07-30 14:57:57 -0400

    Added new command: lock_store

---
+++ django_pph/management/commands/lock_store.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.hashers import get_hasher
+from django.utils.crypto import get_random_string
+
+from django_pph.shamirsecret import ShamirSecret
+from django_pph.settings import SETTINGS
+from django_pph.utils import cache, bin64enc, binary_type
+
+
+class Command(BaseCommand):
+
+    help = 'Forcefully locks the pph store'
+    hasher = get_hasher('pph')
+
+    def handle(self, *args, **options):
+
+        self.lock_store()
+
+
+    def lock_store(self):
+        """
+        Locks a the store for the hasher.
+        """
+        self.hasher.update(
+                shamirsecretobj = None,
+                thresholdlesskey = None,
+                secret = None,
+                is_unlocked = False,
+        )
